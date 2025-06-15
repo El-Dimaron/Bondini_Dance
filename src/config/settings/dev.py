ae9d2d0c64395b42995911ee4bff5a5ccd8672c4
El-Dimaron <el.dimaron.n1@gmail.com>
2025-06-15 23:45:05 +0300

    Added Docker compose and Postgres DB

--- src/config/settings/dev.py
+++ src/config/settings/dev.py
@@ -1,5 +1,7 @@
 import os
 
+from dotenv import load_dotenv
+
 from config.settings.base import *  # NOQA:F403
 
 SECRET_KEY = "django-insecure-&=nand4!!4ul2+8od3u3zws1+je0al^_+-=5jf25^8b_tn*-f*"
@@ -10,11 +12,23 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS += ["django_extensions"]  # NOQA: F405
 
+load_dotenv()
+
+IS_DOCKER = os.environ.get("DOCKER", "0") == "1"
+
 DATABASES = {
-    "default": {
+    "default_sqlite": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",  # NOQA:F405
-    }
+    },
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_DB"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": "postgres" if IS_DOCKER else "localhost",
+        "PORT": os.environ.get("POSTGRES_PORT"),
+    },
 }
 
 STATIC_URL = "static/"
