7cf428670aa865a2fef07ceb783a02c7d5bb0681
El-Dimaron <el.dimaron.n1@gmail.com>
2025-06-18 23:31:51 +0300

    Intermediate changes to shop and 'static' folder

--- src/config/settings/dev.py
+++ src/config/settings/dev.py
@@ -31,8 +31,9 @@ DATABASES = {
     },
 }
 
-STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "common/static"]  # NOQA:F405
+STATIC_URL = "/static/"
+# STATICFILES_DIRS = [BASE_DIR / "common/static"]  # NOQA:F405
+STATICFILES_DIRS = [BASE_DIR / "src", "static"]  # NOQA:F405
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")  # NOQA:F405
